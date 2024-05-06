98ef9223d6819687d1c79b58d8101a38582e6766
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-05-06 10:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+//SGN your JS code here. If required.
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+
+const Prm = () => {
+	let txt = document.getElementById("text").value;
+	let del = parseInt(document.getElementById("delay").value);
+
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(txt);
+		}, del);
+	});
+}
+
+btn.addEventListener('click', () =>{
+	Prm().then(result => {
+		output.textContent = result;
+	}).catch(e =>{
+		console.log('Error:', e);
+	});
+});
+
